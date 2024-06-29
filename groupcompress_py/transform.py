9f4af6f7a07f9f6205b106b7acab0777a8a792a7
Alex Nichol <unixpickle@gmail.com>
2024-06-29 15:42:34 +0100

    start working on pytorch version

---
+++ groupcompress_py/transform.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+
+class Transform(nn.Module):
+    """
+    A permutation of some joint bits in a larger bitmap.
+
+    :param indices: a Long tensor of shape [num_bits]
+    :param perm: a Long tensor of shape [2**num_bits]
+    """
+
+    indices: torch.Tensor
+    perm: torch.Tensor
+
+    def __init__(self, indices: torch.Tensor, perm: torch.Tensor):
+        self.register_buffer("indices", indices)
+        self.register_buffer("perm", perm)
+        self.register_buffer(
+            "bits", 2 ** torch.arange(0, len(indices), device=indices.device)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        :param x: a uint8 [N x K] tensor of booleans.
+        :return: an [N x K] tensor of booleans.
+        """
+        flags = x[:, self.indices]
+        values = torch.einsum("ij,j->i", flags.long(), self.bits)
+        permuted = self.perm[values]
+        out = x.clone()
+        out[:, self.indices] = (permuted.unsqueeze(-1) & self.bits) != 0
+        return out
