ab822dd41b2b0de0f2fe62d5a4d7e71b7975dc8a
Alex Nichol <unixpickle@gmail.com>
2024-06-30 19:22:58 +0100

    draft pytorch search

---
+++ groupcompress_py/entropy_search.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass
+
+import torch
+
+from .transform import Transform
+
+
+@dataclass
+class EntropyResult:
+    transform: Transform
+    delta: float
+
+
+def entropy_search(
+    dataset: torch.Tensor,
+    num_bits: int,
+    num_samples: int,
+    batch_size: int,
+) -> EntropyResult:
+    """
+    :param dataset: [N x K] tensor of booleans.
+    :param num_bits: number of bits to combine.
+    :param num_samples: number of transforms to sample.
+    """
+    result: EntropyResult = None
+    for i in range(0, num_samples, batch_size):
+        n = min(num_samples - i, batch_size)
+        r = _entropy_search(dataset, num_bits, n)
+        if result is None or r.delta > result.delta:
+            result = r
+    return result
+
+
+def _entropy_search(
+    dataset: torch.Tensor,
+    num_bits: int,
+    num_samples: int,
+) -> EntropyResult:
+    """
+    :param dataset: [N x K] tensor of booleans.
+    :param num_bits: number of bits to combine.
+    :param num_samples: number of transforms to sample.
+    """
+    indices = torch.topk(
+        torch.rand(
+            num_samples, dataset.shape[1], device=dataset.device, dtype=torch.float32
+        ),
+        k=num_bits,
+    ).indices  # [num_samples x num_bits]
+    permutations = torch.argsort(
+        torch.rand(num_samples, 2**num_bits, device=dataset.device, dtype=torch.float32)
+    )  # [num_samples x 2**num_bits]
+    bitmask = 2 ** torch.arange(0, num_bits, device=indices.device)
+    old_bits = dataset[:, indices].view(dataset.shape[0], num_samples, num_bits)
+    patterns = torch.einsum("ijk,k->ij", old_bits.long(), bitmask)  # [N x num_samples]
+    permuted = permutations.gather(1, patterns)  # [N x num_samples]
+    new_bits = (permuted[..., None] & bitmask) != 0
+
+    old_ents = _bitwise_entropy(old_bits)
+    new_ents = _bitwise_entropy(new_bits)
+    improvements = old_ents - new_ents
+    best_delta, best_index = improvements.max(0)
+    best_indices = indices[best_index]
+    best_perm = permutations[best_index]
+    return EntropyResult(
+        transform=Transform(best_indices, best_perm),
+        delta=best_delta.item(),
+    )
+
+
+def _bitwise_entropy(bits: torch.Tensor) -> torch.Tensor:
+    """
+    :param bits: [N x S x bits]
+    :return: [S]
+    """
+    sum = bits.float().sum(0)
+    prob = sum / bits.shape[0]
+    ent = (
+        prob * prob.clamp(min=1e-8).log()
+        + (1 - prob) * (1 - prob).clamp(min=1e-8).log()
+    )
+    return -ent.sum(-1)
